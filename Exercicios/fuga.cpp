4879d900d426d68fd5e14a4822041081e552eecf
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-08-10 19:26:01 -0300

    Exercicios

---
+++ Exercicios/fuga.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int h, p, f, d;
+    cin >> h >> p >> f >> d;
+
+    for (int i = 0; i < 16; i++) {
+        f = f + d;
+
+        if (f == 16) f = 0;
+
+        if (f == -1) f = 15;
+
+        if (f == p) {
+            cout << "N\n";
+            return 0;
+        }
+
+        if (f == h) {
+            cout << "S\n";
+            return 0;
+        }
+    }
+
+    return 0;
+}
