a54490ebf794b9dcd05d4c87fceb65c639965f94
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-08-10 17:57:31 -0300

    Exercicios

--- Exercicios/pintando.cpp
+++ Exercicios/pintando.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int main() {
-    float l1, l2, l3;
+    float l1, l2, l3, p;
     cin >> l1 >> l2 >> l3;
 
-    float p = (l1 + l2 + l3) / 2.0;
+    p = (l1 + l2 + l3) / 2.0;
 
     float area = sqrt(p*(p-l1)*(p-l2)*(p-l3));
 
