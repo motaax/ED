d6186f45ecbc1a8ddf46ee90a78724ba35cc173e
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-09-14 14:46:42 -0300

    Aula 9

--- Aula 9/Caixa.h
+++ Aula 9/Caixa.h
@@ -3,13 +3,20 @@
 
 //Classe que modela o TAD Caixa
 class Caixa {
-    
 private:
     double altura = 1.0;
     double largura = 1.0;
     double comprimento = 1.0;
 
 public:
+    Caixa() {} //Construtor default
+
+    Caixa(double a, double l, double c) {
+        altura = a;
+        largura = l;
+        comprimento = c;
+    }
+
     double volume() {
         return altura * largura * comprimento;
     }
--- Aula 9/main.cpp
+++ Aula 9/main.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main() {
     Caixa p1;
+    Caixa p2(100, 2.5, 9.2);
 
-    cout << "Volume da caixa: " << p1.volume() << "\n";
+    cout << "Volume da caixa 1: " << p1.volume() << "\n";
+    cout << "Volume da caixa 2: " << p2.volume() << "\n";
 
     return 0;
 }
