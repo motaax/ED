baa23633ace4c6d0373850cd6ac77e4769a1c095
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-09-23 11:49:02 -0300

    1.cpp

---
+++ Exercicios/Lista - Recursao/1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+//Funcao recursiva
+void inverte_vet(double vet[], int ini, int fim) {
+    //Caso base
+    if(ini >= fim) return;
+
+    //Troca os elementos
+    double aux = vet[ini];
+    vet[ini] = vet[fim];
+    vet[fim] = aux;
+
+    //Chamada recursiva
+    inverte_vet(vet, ini + 1, fim - 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    double vet[n];
+
+    //Leitura do vetor
+    for(int i = 0; i < n; i++) {
+        cin >> vet[i];
+    }
+
+    inverte_vet(vet, 0, n - 1);
+
+    //Imprime o vetor
+    for(int i = 0; i < n; i++) {
+        cout << vet[i] << " ";
+    }
+
+    return 0;
+}
