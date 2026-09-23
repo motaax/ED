dadd28b13b4dda398af4a398fad1f26924975db9
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-09-23 19:11:03 -0300

    Exercicios

---
+++ Exercicios/Lista - Analise de Algoritmo/1.cpp
@@ -0,0 +1,10 @@
+/*
+Linha 2: n + 1
+Linha 3: n^2 + n
+Linha 4: n^2
+Linha 7: 1
+
+f(n) = (n + 1) + (n^2 + n) + n^2 + 1 = 2n^2 + 2n + 2
+
+Complexidade: O(n^2), pois a complexidade e dominada pelo termo de maior grau de f(n).
+*/
