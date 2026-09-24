984bc66d090796c96c5076249b503bac08d1dabf
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-09-24 10:38:20 -0300

    Exercicios

---
+++ Exercicios/Lista - Analise de Algoritmo/4.cpp
@@ -0,0 +1,7 @@
+/*
+Complexidade de pior caso: E o tempo maximo de execucao que um algoritmo necessita para processar uma entrada de tamanho n
+
+Complexidade de melhor caso: E o tempo mínimo de execucao de que o algoritmo precisa para processar uma entrada de tamanho n.
+
+A maior diferenca e no tamanho de entrada.
+*/
