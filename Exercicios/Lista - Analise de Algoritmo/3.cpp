46a556ed3cae69d21a7f279ab13bae3e6b2404ec
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-09-24 10:35:30 -0300

    Exercicios

---
+++ Exercicios/Lista - Analise de Algoritmo/3.cpp
@@ -0,0 +1,7 @@
+/* 
+a(n) < b(n) -> n^2 - n + 549 < 49n + 49
+
+n^2 - 50n + 500 < 0 -> Por Bhaskara: n1 = 13.82 e n2 = 36.18
+
+Logo: E melhor aplicar o Algoritmo A para valores inteiros de n no intervalo: n = 14 e n = 36.
+*/
